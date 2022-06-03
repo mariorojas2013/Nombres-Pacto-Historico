e318ac3c606256e96c73cd919a77de8d27de91d2
mariorojas2013 <mario.rivra@hotmail.com>
2022-06-03 12:58:59 -0500

    work
    work

--- comunidad.php
+++ comunidad.php
@@ -3,19 +3,11 @@
 
 ini_set('display_errors','Off');
 
- 
- 
- 
 $data = leer_fichero_completo("n/nombres.txt");
 $arrayc = explode(";",$data);
 $cont = count($arrayc);
 
  ?>
- <meta property="og:title" content="Crea tu nombre con tematica PACTO HISTORICO">
- <meta property="og:image" id="img_embed" content="">
- <meta property="og:type"  content="website">
- <meta property="og:description" content="Crea tu nombre con tematica PACTO HISTORICO">
-
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <meta http-equiv="Expires" content="0">
  <meta charset="utf-8" >
@@ -27,21 +19,18 @@ $cont = count($arrayc);
 <meta http-equiv="Pragma" content="no-cache">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
 <link rel="stylesheet" type="text/css" href="mini-default.css">
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EK9JLKZLX0"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FYSC715PK3"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-EK9JLKZLX0');
+  gtag('config', 'G-FYSC715PK3');
 </script>
-
- <img src="" class="img_nueva" style="position: absolute;   margin-left: -1000000;">
 <div class="box">
 	<br>
-	<a class="social-btn sb-whatsapp mr-2 mb-2" target="_blank" href="https://web.whatsapp.com/send?text=Si desean tener su nombre estilo pactoHistorico ingresa en https://appsmr.es/pactohistorico/ "> Compartir  <i  class="fa fa-whatsapp" aria-hidden="true"></i></a><br><br>
-	<a class="social-btn sb-facebook mr-2 mb-2" target="_blank" href="http://www.facebook.com/sharer.php?s=100&p[url]=https://appsmr.es/pactohistorico/&p[title]=Nomres pactoHistorico&p[summary]=Si desean tener su nombre estilo pactoHistorico  ">Compartir  <i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+	<a class="social-btn sb-whatsapp mr-2 mb-2" target="_blank" href="https://web.whatsapp.com/send?text=Si desean tener su nombre estilo pactoHistorico ingresa en http://petro.260mb.org/ "> Compartir  <i  class="fa fa-whatsapp" aria-hidden="true"></i></a><br><br>
+	<a class="social-btn sb-facebook mr-2 mb-2" target="_blank" href="http://www.facebook.com/sharer.php?s=100&p[url]=http://petro.260mb.org&p[title]=Nomres pactoHistorico&p[summary]=Si desean tener su nombre estilo pactoHistorico  ">Compartir  <i class="fa fa-facebook-official" aria-hidden="true"></i></a>
 	<br>
 	<div class="input">
 
@@ -49,16 +38,10 @@ $cont = count($arrayc);
 		<br>Plantilla:
     <br>
 
-    <input type="radio" name="selectP" onclick="" id="ph" value="ph" <?php echo ($_GET['op']=="")?'checked':''; ?> <?php echo ($_GET['op']=="ph")?'checked':''; ?> >
+    <input type="radio" name="selectP" onclick="" id="ph" value="ph" <?php echo ($_GET['op']=="ph")?'checked':''; ?> >
     <label for="ph">Pacto historico</label>
     <input type="radio" name="selectP" onclick="" id="ml" value="ml" <?php echo ($_GET['op']=="ml")?'checked':''; ?>>
-    <label for="ml">Con "Me llamo ..."</label>
-    <input type="radio" name="selectP" onclick="" id="ps" value="ps" <?php echo ($_GET['op']=="ps")?'checked':''; ?>>
-    <label for="ps">Con "#Pacto Sabroso"</label>
-    <input type="radio" name="selectP" onclick="" id="cv" value="cv" <?php echo ($_GET['op']=="cv")?'checked':''; ?>>
-    <label for="cv">Con "Cambio por la vida"</label>
-    <input type="radio" name="selectP" onclick="" id="pz" value="pz" <?php echo ($_GET['op']=="pz")?'checked':''; ?>>
-    <label for="pz">Con "se suma al pacto por la paz"</label>
+    <label for="ml">Me llamo</label>
     <br>
 		<input type="text" name="name" id="name" value="<?php echo $_GET['nombre']; ?>"   placeholder="Escribe tu nombre"> 
     <input type="button" value="Enviar" onclick="abrir($('#name').val())" style="font-family: monospace;" >
@@ -72,31 +55,26 @@ $cont = count($arrayc);
 		<div class="render ml mlt">
 		 
 		</div>
-		<img class='ph' src='ph.png'>
-		<img class='ml' src='ml.jpg'>
-		<div class="adicionalText"></div>
+		<div class="bandera">
+		 	 <span class="letra" style="color:#E70000"> -</span><span class="letra" style="color:#FF8C00"> -</span><span class="letra" style="color:#FFEF00"> -</span><span class="letra" style="color:#00811F"> -</span><span class="letra" style="color:#3064A9"> -</span><span class="letra" style="color:#87189D"> -</span> 
+		</div>
+		<!-- <img class='ph' src='ph.png'> -->
+		<!-- <img class='ml' src='ml.jpg'> -->
 		<div class="autor">by MR</div>	
 		 	
 	</div>
 	
 	<input type="button" style="font-family: monospace;" value="Descargar" onclick="downloadimage()">
 	<br><br>
-	<div style="font-family: monospace;"><?php echo $cont ?> frases se han registrado  <i class="fa fa-coffee" aria-hidden="true"></i></div>
+	<div style="font-family: monospace;"><?php echo $cont ?> Personas han participado <i class="fa fa-coffee" aria-hidden="true"></i></div>
 	<br>
 	<br>
-	<div style="font-family: monospace;">Version 0.56</div>
+	<div style="font-family: monospace;">Version 0.49</div>
 	<br>
-	<!-- <div  style="font-family: monospace; color: #c5c5c5;">	Cualquier inquietud escribanos a comentarios_app@outlook.com</div> -->
+	<div  style="font-family: monospace; color: #c5c5c5;">	Cualquier inquietud escribanos a comentarios_app@outlook.com</div>
 	<br>
-	<form action="?mensaje=ok" method="post">
-		<label style="font-family: monospace;">Déjanos tus comentarios!</label><br>
-		<textarea name="comentario"></textarea><br>
-		<input type="submit" style="font-family: monospace;" value="comentar">
-		<div><?php echo $_GET['m']; ?></div>
-	</form>
 </div>	
-
-
+ 
 
 <style type="text/css">
 	html, * {
@@ -136,9 +114,18 @@ $cont = count($arrayc);
 		    padding: 64px;
 		    padding-left: 6px;
 	}
+	.bandera{
+		font-size: 7em;
+	    font-family: 'Arial Black';
+	    text-transform: uppercase;
+	    margin-left: 101px;
+	    width: 1000px;
+
+
+	}
 	.render.ph{
 		font-size: 7em;
-	    font-family: 'petroFont','Arial Black';
+	    font-family: 'Arial Black';
 	    text-transform: uppercase;
 	    margin-left: 101px;
 	    width: 1000px;
@@ -147,7 +134,7 @@ $cont = count($arrayc);
 		 position: relative;
 	    color: #952385;
 	    font-size: 6em;
-	    font-family: 'petroFont','Arial Black';
+	    font-family: 'Arial Black';
 	    text-transform: uppercase;
 	    top: 197px;
 	    margin: -106px auto;
@@ -171,9 +158,9 @@ $cont = count($arrayc);
 		    background: #15427a;
 		}
 	.letra {
-		font-family: 'petroFont','Arial Black';
-    	margin-left: -58px;
-    	opacity: 0.9;
+		    font-family: arial black;
+    	    margin-left: -40px;
+    	/*opacity: 0.9;*/
 	}
 	.letra_esp{
 		margin-left: 43px;
@@ -192,41 +179,8 @@ $cont = count($arrayc);
 		    z-index: 1000000000000000;
 		    opacity: 0.9;
 	}
-	.pz{
-		    position: relative;
-		    top: -143px;
-		    margin: 0 auto;
-		    color: #ad439e;
-		    font-size: 1.2em;
-		    padding-left: 75px;
-		    /* width: 10px; */
-		    font-family: 'petroFont';
-	}
-	.cv{
-		position: relative;
-	    top: -143px;
-	    letter-spacing: 4px;
-	    margin: 0 auto;
-	    color: #1c3587;
-	    font-size: 1.2em;
-	    /* width: 10px; */
-	    padding-left: 51px;
-	    font-family: system-ui;
-	    font-weight: 700;
-	}
-	.ps{
-		position: relative;
-	    top: -143px;
-	    margin: 0 auto;
-	    color: #ad439e;
-	    font-size: 1.2em;
-	    width: 10px;
-	    padding-right: 102px;
-	    font-family: 'petroFont';
-	}
-
 	@font-face {
-	  font-family: 'petroFont';
+	  font-family: 
 	  src: url('GothamBold.ttf') format('truetype');
 	}
 </style>
@@ -239,39 +193,31 @@ $cont = count($arrayc);
 
 	function cambio(){
 		var opcion = $('input:radio[name=selectP]:checked').val();
-		if(opcion == "ph" || opcion == "ps" || opcion == "cv" || opcion == "pz"){
+		if(opcion == "ph"){
 			$('.ph').show();
 			$('.ml').hide();
-			$('.ps').show();
-			$('.cv').show();
-			$('.pz').show();
 		}
 		if(opcion == "ml"){
 			$('.ml').show();
 			$('.ph').hide();
-			$('.ps').hide();
-			$('.cv').hide();
-			$('.pz').hide();
 		}
-		 
 	}
 
 	function abrir(nombre){
 		var opcion = $('input:radio[name=selectP]:checked').val();
-		window.open("?nombre="+nombre+"&op="+opcion,"_self");
+		window.open("?nombre="+nombre+"&op="+opcion,"_self")
 
 	}
 	function rev(){
 		var opcion = $('input:radio[name=selectP]:checked').val();
-		// if(opcion != "ml"){ 
-			
-		// }
+		if(opcion != "ml"){ 
+			$("#ph").prop("checked", true);
+		}
 	}
 		
 rev();
 convertir();
 cambio();
-compilar();
 	function downloadimage() {
                 /*var container = document.getElementById("image-wrap");*/ /*specific element on page*/
                 var container = document.getElementById("design");; /* full page */
@@ -283,69 +229,25 @@ compilar();
                     link.href = canvas.toDataURL();
                     link.target = '_blank';
                     link.click();
-
                 });
      }
 
-     function compilar(){
-     			 var container = document.getElementById("design");; /* full page */
-                html2canvas(container, { allowTaint: true }).then(function (canvas) {
-
-                    var link = document.createElement("a");
-                    document.body.appendChild(link);
-                    link.download = $('#name').val() +".jpg";
-                    link.href = canvas.toDataURL();
-                    link.target = '_blank';
-                    // link.click();
-                     
-                    $('.img_nueva').attr('src',link.href);
-                    
-                });
-     }
-
-
-
-
- 
 
  var textoA;
  function convertir(){
- 	// $("#ph").prop("checked", true);
  	var opcion = $('input:radio[name=selectP]:checked').val();
  	var data =  '<?php echo $_GET['nombre']; ?>';
-
- 	if(opcion == "pz")
- 	{
- 		$('.adicionalText').html("se suma al pacto por la Paz");
- 		$('.adicionalText').addClass("pz");
-
- 	}
- 	if(opcion == "ps")
- 	{
- 		$('.adicionalText').html("#PACTOSABROSO");
- 		$('.adicionalText').addClass("ps");
-
- 	}
- 	if(opcion == "cv")
- 	{
- 		$('.adicionalText').html("CAMBIO POR LA VIDA");
- 		$('.adicionalText').addClass("cv");
- 	}
- 	if(opcion == "ph" || opcion == "ps" || opcion == "cv" || opcion == "pz"){
+ 	if(opcion == "ph"){
 			textoA = Array.from(data);
 				//amarillo, azul, rojo, verde, fuscia
-				coloresE = ['#f7a600','#164194','#e41513','#32a337','#942685'];
+				coloresE = ['#E70000','#FF8C00','#FFEF00','#00811F','#3064A9','#87189D'];
 				render = "";
 				co= 0;
 				// EnO = textoA.indexOf("o");
 				// console.log("o:::",);
 			    textoA.forEach( function(valor, indice, array) {
 			      console.log(co + "::" + valor);
-			      if(valor == "o" && textoA.length-1 == indice){
-			      	render =  render +  "<img class='o' src='o.png'>" ;
-			        $('.render').html(render);
-			      }
-			      else{
+			        
 			      	if(valor == " "){
 			      		render =  render +  "<span class='letra_esp' style='color:"+coloresE[co]+"'> "+valor+"</span>" ;
 			      	}
@@ -354,11 +256,10 @@ compilar();
 			      		render =  render +  "<span class='letra' style='color:"+coloresE[co]+"'> "+valor+"</span>" ;
 			      	}
 			      	
-			      	if(textoA.length-1 == indice){
-			      	   render =  render +  "<img class='o_d' src='o.png'>" ;	
-			      	}
+			      	 
 			        $('.render').html(render);
-			      }
+
+			      
 			      co = control(co);
 			  });
  	}
@@ -367,26 +268,19 @@ compilar();
  	{
  		$('.render').html(data);
  	}
- 	
- 	
 	
 	
  }
  
 function control(i) { 
 	r = i + 1;
-	if(i == 4)
+	if(i == 5)
 	r = 0;
     return r;
 } 
 
-
 	
 </script>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1279339354929382"
-     crossorigin="anonymous"></script>
-
-
 <?php 
 
 $nombre = $_GET['nombre'];
@@ -398,17 +292,8 @@ if($nombre != ""){
      file_put_contents($nombreArchivo, $datos, FILE_APPEND);
 }
 
-$mensaje = $_GET['mensaje'];
-$comentario = $_POST['comentario'];
-$nombreArchivos = "n/comentarios.txt";
-if($mensaje == "ok")
-{
-	if($comentario != ""){
-	  $datoss = $datoss ."||".$comentario ;
-     file_put_contents($nombreArchivos, $datoss, FILE_APPEND);
-      echo "<script>window.location.replace('http://".$_SERVER['SERVER_NAME']."?m=Gracias');</script>";
-	}
-}
+
+	
  
 
 
--- leer.php
+++ leer.php
@@ -3,9 +3,29 @@
 
 $comando = $_GET['servicio'];
 if($comando == "Col8080"){
-	echo leer_fichero_completo("n/nombres.txt");
-}
+	$data = leer_fichero_completo("n/nombres.txt");
+	// echo $data;
+	$arrayc = explode(";",$data);
+	echo "<br>";
+	echo count($arrayc);
+	echo "<br> <script>
+window.setTimeout( function() {
+  window.location.reload();
+}, 30000);
+</script>";
+	print("<pre>".print_r(array_reverse($arrayc),true)."</pre>");
 
+}
+if($comando == "Col9090"){
+	$data = leer_fichero_completo("n/comentarios.txt");
+	// echo $data;
+	$arrayc = explode("||",$data);
+	echo "<br>";
+	echo count($arrayc);
+	echo "<br>";
+	print("<pre>".print_r(array_reverse($arrayc),true)."</pre>");
+
+}
 
 function leer_fichero_completo($nombre_fichero){
    //abrimos el archivo de texto y obtenemos el identificador
